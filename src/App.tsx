5b30812201122cf1439f53bfbd8b79bda2975fa6
Thijs Kraaijenbos <thijs.kraaijenbos@notive.nl>
2026-01-13 17:58:50 +0100

    Backup commit random tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {Card} from "./components/Card.tsx";
 
 function App() {
 
   return (
     <>
+      <Card background={true}>
+        <p className={"bg-red-500"}>test</p>
+      </Card>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/kritische-literatuurstudie-notive"
+  base: process.env.VITE_BASE_PATH || "/kritische-literatuurstudie-notive/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
